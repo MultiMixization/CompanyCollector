2866821da5aaae7e98a08c73968b42418ae5bc0e
MultiMixization <45719889+MultiMixization@users.noreply.github.com>
2021-02-12 20:38:29 +0100

    Getting words from target site not working

--- CompanyCollector/Companies.cs
+++ CompanyCollector/Companies.cs
@@ -10,30 +10,34 @@ namespace CompanyCollector
 {
     public class Companies
     {
+        private ChromeOptions _options;
         private IWebDriver _driver;
         private DelLogger _logger;
         public Companies(DelLogger logger)
         {
+            //Initialize options for Driver
+            _options = new ChromeOptions();
+            _options.AddArguments(/*"headless",*/"--blink-settings=imagesEnabled=false");
 
             //Initialize the instance
-            _driver = new ChromeDriver();
+            _driver = new ChromeDriver(_options);
             _logger = logger;
         }
 
 
         public List<string> GetCompanies(int pageAmount)
         {
-            string url = "https://www.europages.de/unternehmen/Produktion.html";
+            string url = "https://www.europages.de/unternehmen/Deutschland/fenster.html";
 
             var companyList = new List<string>();
 
             try
             {
-                for (int i = 1; i < 1000; i++)
+                for (int i = 0; i < pageAmount; i++)
                 {
-                    if (i > 1)
+                    if (i > 0)
                     {
-                        companyList.AddRange(GetCompaniesFromPage($"https://www.europages.de/unternehmen/pg-{i}/Produktion.html"));
+                        companyList.AddRange(GetCompaniesFromPage($"https://www.europages.de/unternehmen/pg-{i}/Deutschland/fenster.html"));
                     }
                     else
                     {
@@ -66,25 +70,44 @@ namespace CompanyCollector
             _driver.Navigate().GoToUrl(url);
 
             //maximize the browser
-            _driver.Manage().Window.Minimize();
+            //_driver.Manage().Window.Minimize();
 
             //find the element by xpath and enter the email address which you want to login.
             //driver.FindElement(By.XPath("//input[@aria-label='Email or phone']")).SendKeys("email adress);
 
             //wait for a seconds
-            Task.Delay(1000).Wait();
+            Task.Delay(200).Wait();
 
             //find the Next Button and click on it.
+            
+            var pages = _driver.FindElements(By.XPath("//a[contains(@class,'company-name')]"));
 
-            var all = _driver.FindElements(By.XPath("//a[contains(@class,'company-name')]"));
+             List<string> lst = new List<string>();
 
-            List<string> lst = new List<string>();
-            foreach (var a in all)
+            foreach(var a in pages)
             {
-                lst.Add(a.Text);
-            }
+                var temp = _driver.CurrentWindowHandle;
+                string link = a.GetAttribute("href");
+                ((IJavaScriptExecutor)_driver).ExecuteScript("window.open();");
+                _driver.SwitchTo().Window(_driver.WindowHandles[1]);
+                _driver.Navigate().GoToUrl(link);
+                Task.Delay(100).Wait();
+                lst.Add(string.Join(",", GetDataPerCompany()));
+                _driver.Close();
+                _driver.SwitchTo().Window(_driver.WindowHandles[0]);
+                Task.Delay(100).Wait();
+            }          
 
             return lst;
+        }
+
+        private List<string> GetDataPerCompany()
+        {
+            List<string> line = new List<string>();
+            line.Add(_driver.FindElement(By.XPath("//div[contains(@class, 'company-content')]/h3")).Text);
+            line.Add(_driver.FindElement(By.XPath("//dd[contains(@class, 'company-country')]/span")).Text);
+            line.Add(_driver.FindElement(By.XPath("//dd[contains(@itemprop, 'addressLocality')]/pre")).Text);
+            return line;
         }
 
     }
